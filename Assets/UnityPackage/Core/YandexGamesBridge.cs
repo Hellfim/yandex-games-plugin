b88b146369bf0e51404141cfbba75c99c81e4f0a
Hellfim <hellyforex@gmail.com>
2023-12-29 19:24:40 +0300

    Added C# bridge class and updated editor asmdef

---
+++ Assets/UnityPackage/Core/YandexGamesBridge.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UnityPackage.Core
+{
+    public static class YandexGamesBridge
+    {
+        [DllImport("__Internal")]
+        private static extern void TestMethod();
+    }
+}
