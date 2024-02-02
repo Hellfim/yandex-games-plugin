a1bbae0bae462344bc5dc8c8c434705e5bbd7d64
Hellfim <hellyforex@gmail.com>
2024-02-03 01:58:47 +0300

    Removed aspect ratio lock and updated package

--- Assets/UnityPackage/WebGLTemplates/Yandex/canvas-size-processor.js
+++ Assets/UnityPackage/WebGLTemplates/Yandex/canvas-size-processor.js
@@ -1,15 +1,6 @@
 function updateCanvasSize() {
-    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    const targetAspectRatio = 390/844;
-    if ((vw/vh) > targetAspectRatio * 2) {
-        canvas.style.height = "100%";
-        canvas.style.width = vh * (targetAspectRatio) + "px";
-    }
-    else {
-        canvas.style.height = "100%";
-        canvas.style.width = "100%";
-    }
+    canvas.style.height = "100%";
+    canvas.style.width = "100%";
 }
 
 updateCanvasSize();
