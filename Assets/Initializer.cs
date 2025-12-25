88e139203a8f62c49870b5f919c67014ea5d8ae4
Hellfim <hellyforex@gmail.com>
2025-12-26 02:15:25 +0300

    Updated Unity versions

---
+++ Assets/Initializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using YandexGamesPlugin.Core;
+
+namespace DefaultNamespace
+{
+    public class Initializer : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Starting");
+            
+            YandexGamesBridgeEventsListener.InitializationFinished += OnInitializationFinished;
+            YandexGamesBridge.Initialize();
+        }
+
+        private void OnDestroy()
+        {
+            YandexGamesBridgeEventsListener.InitializationFinished -= OnInitializationFinished;
+        }
+
+        private void OnInitializationFinished(Boolean result)
+            => Debug.Log(result ? "Success!" : "Failed!");
+    }
+}
